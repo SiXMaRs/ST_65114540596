87a933855c1c6a41208d789c3bd3aef48c898881
SiXMaRs <wuttinan.lu.65@ubu.ac.th>
2025-08-20 11:19:46 +0700

    addhost

--- final/settings.py
+++ final/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     # "django_browser_reload.middleware.BrowserReloadMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,6 +89,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
+def _split_csv(v, default=""):
+    return [x.strip() for x in (v or default).split(",") if x.strip()]
+
+ALLOWED_HOSTS = _split_csv(os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,202.28.49.122"))
+
+# CSRF ให้ไว้ทั้ง http/https
+CSRF_TRUSTED_ORIGINS = [f"http://{h}" for h in ALLOWED_HOSTS] + [f"https://{h}" for h in ALLOWED_HOSTS]
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -100,7 +110,12 @@ DATABASES = {
     }
 }
 
-# Password validation
+if not DEBUG:
+    SESSION_COOKIE_SECURE = os.getenv("DJANGO_SECURE_COOKIES","true").lower()=="true"
+    CSRF_COOKIE_SECURE = os.getenv("DJANGO_SECURE_COOKIES","true").lower()=="true"
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")  
+
+# Password validation 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -136,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
